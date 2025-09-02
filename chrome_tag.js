faff288ae553ea8b5929d8662da5c72d113f49c9
seacvr <134600598+seancvr@users.noreply.github.com>
2025-09-02 15:02:35 -0600

    updated find tags function

--- chrome_tag.js
+++ chrome_tag.js
@@ -4,7 +4,7 @@ idTagRegex = /id=(GTM-[A-Z0-9]+|G-[A-Z0-9]+|UA-[A-Z0-9-]+|AW-[0-9]+)(?=&|$)/;
 let tagData = {}
 
 // this line failed on a page and the script crashed
-const scriptTagNodeList = document.querySelectorAll("script") 
+ 
 
 function getTagId(url) {
     if (idTagRegex.test(url)) {
@@ -23,17 +23,22 @@ function renderArray(tags) {
   ulEl.innerHTML = listItems
 }
 
-// get googletagmanager src url's
-scriptTagNodeList.forEach(element => { 
-    srcUrl = element.getAttribute("src")
+function findTags() {
+    const scriptTagNodeList = document.querySelectorAll("script")
+    // get googletagmanager src url's
+    scriptTagNodeList.forEach(element => {
+        srcUrl = element.getAttribute("src")
 
-    // if src = '', attr contains null
-    if (srcUrl) {
-        if (srcUrl.includes("googletagmanager")) {
-            getTagId(srcUrl)
+        // if src = '', attr contains null
+        if (srcUrl) {
+            if (srcUrl.includes("googletagmanager")) {
+                getTagId(srcUrl)
+            }
         }
-    }
-})
+    })
+}
+
+
 
 console.log(tagIdList)
 //capture the tags the regex misses and save for later analysis
