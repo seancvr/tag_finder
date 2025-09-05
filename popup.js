cf6e1e549292aec9275dec120899195c61139aa4
seacvr <134600598+seancvr@users.noreply.github.com>
2025-09-05 17:03:00 -0600

    moved regex parser to separate file

--- popup.js
+++ popup.js
@@ -1,17 +1,6 @@
+import getTagId, { idTagRegex } from './utils/parser.js';
 let googleTagData = []
 let unmatchedUrlList = []
-/*
-Chrome tag formats:
-// AW-xxxx, DC-xxxx, G-xxxx, GT-xxx, GTM-xxxx, PUB-xxxx and UA-xxx-x
-
-Regex:
-GTM-[A-Z0-9]+ matches GTM- followed by one or more (+) uppercase letters/digits.
-G-[A-Z0-9]+ matches G- followed by one or more (+) uppercase letters/digits.
-UA-[A-Z0-9-]+ matches UA- followed by on or more (+) uppercase letters, digits, or dashes.
-(?=&|$) This is a lookahead assertions. Checks that what follows is either a & (start of another query parameter), 
-or the end of the string ($), but does not include them in the match.
-*/
-let idTagRegex = /id=(GTM-[A-Z0-9]+|G-[A-Z0-9]+|UA-[A-Z0-9-]+|AW-[0-9]+)(?=&|$)/
 
 // add onclick event listener to the button element in popop.html
 document.querySelector("#button-el").addEventListener("click", async function() {
@@ -37,8 +26,8 @@ document.querySelector("#button-el").addEventListener("click", async function() 
   }
 
   // extract tagID's from srcUrls and make a note of the unmatched tags for later analysis
-  const tagIds = scriptData.srcUrls.map(url => getTagId(url)).filter(id => id !== null)
-  unmatchedUrlList =  scriptData.srcUrls.filter(url => getTagId(url) === null)
+  const tagIds = scriptData.srcUrls.map(url => getTagId(url, idTagRegex)).filter(id => id !== null)
+  unmatchedUrlList =  scriptData.srcUrls.filter(url => getTagId(url, idTagRegex) === null)
   console.log(unmatchedUrlList) // for debugging
   
   // put useful data in an object and push to googleTagData
@@ -87,17 +76,6 @@ function getDOMdata() {
     } catch (error) {
         console.log(error.message) // for debug
         return { error: error.message };
-    }
-}
-
-// extract the tag ID from the src url string
-function getTagId(url) {
-    if (idTagRegex.test(url)) {
-        let extractedId = url.match(idTagRegex)
-        return extractedId[1]
-    } else {
-      // if no match
-        return null
     }
 }
 
