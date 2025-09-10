f03afef6616e768ed24aaa87e0d961bc61b94954
seacvr <134600598+seancvr@users.noreply.github.com>
2025-09-09 20:37:16 -0600

    update storage functions

--- utils/storage.js
+++ utils/storage.js
@@ -2,12 +2,26 @@
 export function loadGoogleTagData() {
   // pass [] as default if storage is empty
   return chrome.storage.local
-    .get({googleTagData: []})
+    .get({ googleTagData: [] })
     .then(result => result.googleTagData);
 }
 
 // save data to extension storage
-export function storeGoogleTagData(data) {
-  return chrome.storage.local
-    .set({ "googleTagData": data })
+export function storeGoogleTagData(newPageData) {
+  // first check if that page data has already been saved
+  dataFromStorage = loadGoogleTagData()
+
+  // check if array is empty
+  if (dataFromStorage > 0) {
+    // return true if there is a match
+    if (
+      dataFromStorage.some(item =>
+        item.pageUrl === newPageData.pageUrl
+      )
+    ) {
+      return chrome.storage.local
+        .set({ "googleTagData": newPageData })
+    }
+  }
 }
+
