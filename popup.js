3f1ec055bb2decc45adc01f5df7a8dc4c56ba8df
seacvr <134600598+seancvr@users.noreply.github.com>
2025-09-10 20:08:48 -0600

    store empty list on first popup load

--- popup.js
+++ popup.js
@@ -17,6 +17,7 @@ document.addEventListener("DOMContentLoaded", async function () {
   if (googleTagData.length > 0) {
     renderGoogleTagData(googleTagData)
   }
+  storeGoogleTagData(googleTagData)
 })
 
 // add onclick event listener to the button element
