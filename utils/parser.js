e7a1de129d3f78f6781489380571d0c8aec9afb7
seacvr <134600598+seancvr@users.noreply.github.com>
2025-09-13 22:34:44 -0600

    updated regex

--- utils/parser.js
+++ utils/parser.js
@@ -9,8 +9,8 @@ UA-[A-Z0-9-]+ matches UA- followed by on or more (+) uppercase letters, digits, 
 (?=&|$) This is a lookahead assertions. Checks that what follows is either a & (start of another query parameter), 
 or the end of the string ($), but does not include them in the match.
 */
-// export const idTagRegex = /id=(GTM-[A-Z0-9]+|G-[A-Z0-9]+|UA-[A-Z0-9-]+|AW-[0-9]+|DC-[0-9]+)(?=&|$)/
-export const idTagRegex = /id=(GTM-[A-Z0-9]+|G-[A-Z0-9]+|UA-[A-Z0-9-]+|AW-[0-9]+)(?=&|$)/
+
+export const idTagRegex = /id=(GTM-[A-Z0-9]+|G-[A-Z0-9]+|UA-[A-Z0-9-]+|AW-[0-9]+|DC-[0-9]+)(?=&|$)/
 
 // extract the tag ID from the src url string
 export function getTagId(url, regex) {
