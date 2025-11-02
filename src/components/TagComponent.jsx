32cb4005a4a3c773e25c34cce68b591ed6be6b8b
seacvr <134600598+seancvr@users.noreply.github.com>
2025-11-01 23:55:18 -0600

    added redirect functionality to links

--- src/components/TagComponent.jsx
+++ src/components/TagComponent.jsx
@@ -1,10 +1,14 @@
 export default function TagComponent({ data, onRemoveEntry }) {
+  const baseUrl = "https://search.dnslytics.com/search?d=domains&q=";
+
   // render using ternary operator to check if gtags = [] empty array
   const linksHtml =
     data.gtags.length > 0
       ? data.gtags.map((tagId, index) => (
           <span key={tagId}>
-            <a>{tagId}</a>
+            <a href={baseUrl + tagId} target="_blank" rel="noopener noreferrer">
+              {tagId}
+            </a>
             {/* add commas except for the last element of the array */}
             {index < data.gtags.length - 1 && ", "}
           </span>
